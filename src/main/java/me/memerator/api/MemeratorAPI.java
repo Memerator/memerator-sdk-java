c95fe1997d97b9dd26076fd826ff6a1d34c97232
Chew <chew@chew.pw>
2020-06-24 18:11:53 -0500

    Add Javadoc comments for main class

--- src/main/java/me/memerator/api/MemeratorAPI.java
+++ src/main/java/me/memerator/api/MemeratorAPI.java
@@ -20,19 +20,35 @@ public final class MemeratorAPI {
         api = new API(apiKey);
     }
 
+    /**
+     * @return the token you entered
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * Changes the token
+     * @param newToken the new token
+     */
     public void setToken(String newToken) {
         token = newToken;
         api = new API(newToken);
     }
 
+    /**
+     * The API method used in the lib. Has base URL already set.
+     * @return the API
+     */
     public API getAPI() {
         return api;
     }
 
+    /**
+     * Gets a meme from the website by its Meme ID.
+     * @param id the meme id
+     * @return a Meme
+     */
     public Meme getMeme(String id) {
         try {
             return new Meme(new JSONObject(getAPI().get("meme/" + id)));
@@ -41,26 +57,54 @@ public final class MemeratorAPI {
         }
     }
 
+    /**
+     * Gets a user by username or user ID.
+     * @param username the username or ID.
+     * @return a User
+     */
     public User getUser(String username) {
         return new User(new JSONObject(getAPI().get("profile/" + username)));
     }
 
+    /**
+     * Gets the currently logged in Profile. Inherits User
+     * @return your Profile
+     */
     public Profile getProfile() {
         return new Profile(new JSONObject(getAPI().get("profile/me")));
     }
 
+    /**
+     * The site's stats as seen on https://memerator.me/stats
+     * @return a Stats object with the stats.
+     */
     public Stats getStats() {
         return new Stats(new JSONObject(getAPI().get("stats")));
     }
 
+    /**
+     * Gets a random meme from the website. Up to Teen by default.
+     * @return a random meme
+     */
     public Meme getRandomMeme() {
-        return new Meme(new JSONObject(getAPI().get("meme/random?age=2")));
+        return getRandomMeme(MaxAge.TEEN);
     }
 
+    /**
+     * Gets a random meme from the website up a specified Max Age.
+     * @param max the maximum allowed age group.
+     * @return a Meme.
+     */
     public Meme getRandomMeme(MaxAge max) {
         return new Meme(new JSONObject(getAPI().get("meme/random?age=" + max.getAgeInt())));
     }
 
+    /**
+     * Searches the website for a meme. As seen on https://memerator.me/search
+     * @param query the search query
+     * @return an Array of Memes. Can be empty, never null.
+     * @throws UnsupportedEncodingException this will never be thrown.
+     */
     public Meme[] searchMemes(String query) throws UnsupportedEncodingException {
         JSONArray memeresponse = new JSONArray(getAPI().get("meme/search?search=" + URLEncoder.encode(query, "UTF-8")));
         if(memeresponse.length() > 0) {
@@ -75,6 +119,11 @@ public final class MemeratorAPI {
         }
     }
 
+    /**
+     * Submit a meme. See MemeBuilder class.
+     * @param meme a prepared Meme
+     * @return the submitted Meme.
+     */
     public Meme submitMeme(JSONObject meme) {
         if(meme.has("url")) {
             return getMeme(new JSONObject(getAPI().post("/submit", (HashMap<String, Object>) meme.toMap())).getString("memeid"));
