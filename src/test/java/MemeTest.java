84877866d1f1739b3ac220b8033938b5150c698e
Chew <chew@chew.pw>
2020-08-27 23:06:14 -0500

    More test assertions

--- src/test/java/MemeTest.java
+++ src/test/java/MemeTest.java
@@ -25,5 +25,14 @@ public class MemeTest {
         Meme meme = api.getMeme("aaaaaaa");
         assertNotNull(meme.getCaption());
         assertEquals("Chew", meme.getAuthor().getUsername());
+        assertEquals("***__AAAAAAAA__***", meme.getCaption());
+        assertFalse(meme.isDisabled());
+        assertTrue(meme.getAverageRating() >= 1 && meme.getAverageRating() <= 5);
+        assertTrue(meme.getTotalRatings() >= 1);
+        assertEquals(Age.FAMILY_FRIENDLY, meme.getAgeRating());
+        assertEquals("https://cdn.memerator.me/K7bLRy9.jpg", meme.getImageUrl());
+        assertEquals("https://memerator.me/meme/" + meme.getMemeId(), meme.getMemeUrl());
+        assertEquals(1550237341, meme.getTimestamp().toEpochMilli() / 1000);
+        assertEquals("over 1 year", meme.getTimeAgoFormatted());
     }
 }
